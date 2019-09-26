4698b2b946632ea0137c5433b260084f6f47e452
Chris Lewark <chris.lewark@gmail.com>
2019-09-26 17:50:22 -0400

    Initial Commit

---
+++ BetterNutrientPasteDispenserControls/Patches.cs
@@ -0,0 +1,80 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+
+namespace BetterNutrientPasteDispenserControls
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            //Log.Message("Hello from Harmony in scope: com.github.harmony.rimworld.maarx.betternutrientpastedispensercontrols");
+            var harmony = HarmonyInstance.Create("com.github.harmony.rimworld.maarx.betternutrientpastedispensercontrols");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    //public override IEnumerable<Gizmo> GetGizmos()
+    [HarmonyPatch(typeof(Building))]
+    [HarmonyPatch("GetGizmos")]
+    class Patch_Building_GetGizmos
+    {
+        static void Postfix(Building __instance, ref IEnumerable<Gizmo> __result)
+        {
+            if (__instance is Building_NutrientPasteDispenser)
+            {
+                Log.Message("Hello from Harmony Postfix Patch_Building_GetGizmos " + __result.Count());
+
+                //Building_NutrientPasteDispenser myThis = (Building_NutrientPasteDispenser)__instance;
+                //MethodInfo CanDispenseNow = myThis.GetType().GetMethod("CanDispenseNow", BindingFlags.Instance | BindingFlags.Public);
+                //MethodInfo TryDispenseFood = myThis.GetType().GetMethod("TryDispenseFood", BindingFlags.Instance | BindingFlags.Public);
+
+                //int[] numbers = { 1, 5, 25 };
+                //foreach (int i in numbers)
+                //{
+                //    Gizmo newGizmo = new Command_Action
+                //    {
+                //        defaultLabel = "Dispense " + i,
+                //        action = delegate
+                //        {
+                //            int k = i;
+                //            for (int j = 0; j < k && (bool) CanDispenseNow.Invoke(myThis, null); j++)
+                //            {
+                //                TryDispenseFood.Invoke(myThis, null);
+                //            }
+                //        }
+                //    };
+                //    __result.Add(newGizmo);
+                //}
+
+                Building_NutrientPasteDispenser myThis = (Building_NutrientPasteDispenser)__instance;
+
+                int[] numbers = { 1, 5, 25 };
+                foreach (int i in numbers)
+                {
+                    Gizmo newGizmo = new Command_Action
+                    {
+                        defaultLabel = "Dispense " + i,
+                        action = delegate
+                        {
+                            for (int j = 0; j < i && myThis.CanDispenseNow; j++)
+                            {
+                                Thing meal = myThis.TryDispenseFood();
+                                GenPlace.TryPlaceThing(meal, myThis.InteractionCell, myThis.Map, ThingPlaceMode.Near);
+                            }
+                        }
+                    };
+                    __result = __result.Add(newGizmo);
+                }
+                Log.Message("Goodbye from Harmony Postfix Patch_Building_GetGizmos " + __result.Count());
+            }
+        }
+    }
+
+}
